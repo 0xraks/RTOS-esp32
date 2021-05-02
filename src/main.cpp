bb631eafb3944152527e43ba106c2e0518753a31
rakshith <rakshith152@gmail.com>
2021-05-02 13:24:03 +0530

    Initialized repo and added basic RTOS example

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+const int led1 = 2; // Pin of the LED
+
+void toggleLED(void * parameter) {
+  for (;;) {
+    digitalWrite(led1, HIGH);
+    Serial.println("Turning on LED");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    digitalWrite(led1, LOW);
+    Serial.println("Turning off LED");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  xTaskCreate(
+    toggleLED,    // Function that should be called
+    "Toggle LED",   // Name of the task (for debugging)
+    1000,            // Stack size (bytes)
+    NULL,            // Parameter to pass
+    1,               // Task priority
+    NULL             // Task handle
+  );
+}
+
+void loop() {
+}
